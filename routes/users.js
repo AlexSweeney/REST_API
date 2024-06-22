ecdb73b39be6f478f194161be348b564b0190f4f
Alex Sweeney <a.sweeney@ryte.com>
2024-06-22 20:08:24 +0100

    progress: 39:10

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,28 @@
 import express from 'express';
+import { v4 as uuid } from 'uuid';
+uuid()
 
 const router = express.Router();
 
-const users = [
-  {
-    name: "John",
-    lastName: "doe",
-    age: 25
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24
-  }
-];
+const users = [];
 
 router.get('/', (req, res) => {
   console.log(users);
   res.send(users)
 });
 
-router.post('/', (req, res) => {
-  console.log("POST ROUTE REACHED")
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
 
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+});
+
+router.post('/', (req, res) => {
   const user = req.body;
 
-  users.push(user)
+  users.push({ ...user, id: uuid() })
 
   res.send(`user with name: ${user.firstName} added to database!`);
 });
