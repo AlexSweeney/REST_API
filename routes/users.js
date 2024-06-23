739bc4672a40797c937c7734d323cd232c0c5d71
Alex Sweeney <a.sweeney@ryte.com>
2024-06-23 12:23:58 +0100

    progress: 51:20

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ uuid()
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 router.get('/', (req, res) => {
   console.log(users);
@@ -19,6 +19,14 @@ router.get('/:id', (req, res) => {
   res.send(foundUser);
 });
 
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with id: ${id} deleted from database.`)
+});
+
 router.post('/', (req, res) => {
   const user = req.body;
 
@@ -26,5 +34,26 @@ router.post('/', (req, res) => {
 
   res.send(`user with name: ${user.firstName} added to database!`);
 });
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+
+  const user = users.find((user) => user.id === id );
+
+  if(firstName) {
+    user.firstName = firstName;
+  }
+
+  if(lastName) {
+    user.lastName = lastName;
+  }
+
+  if(age) {
+    user.age = age;
+  }
+
+  res.send(`User with the id: ${id} has been updated`)
+})
 
 export default router;
